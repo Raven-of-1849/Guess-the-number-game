beca815c2244a1b0eee854e0f8ca40296b067f25
Raven-of-1849 <75646767+Raven-of-1849@users.noreply.github.com>
2023-11-12 23:38:58 -0600

    Remove commented out code, remove duplicate guess count

--- main.c
+++ main.c
@@ -3,105 +3,93 @@
 #include <time.h>
 #include <string.h>
 
-// New Challange
-// Rules 
-    //Generate number from 0-20
-    //After generation, ask user to guess the number; User to only be able to enter number from 0-20, else error.
-    //After succesful guess, return user message, too low or too high
-    //User wins if they guess the correct number within 5 attemps.
-    // Use if statements and while statements
-
 int main() {
         
-        int userNumber; // User entered number
-        int guessCount = 5; // Number of guesses 
-        int remainingGuesses = 5;
-        float tempNum;
+        float tempNum;              // User entered input
+        int userNumber;             // Convert tempNumber to user entered number
+        int guessCount = 5;         // Number of guesses 
         
         
         time_t t; //Time variable
-        srand( (unsigned) time(&t) ); // initialize the random number generator
-        int randomNumber =  rand() % 21; // Get random number from 0-20
+        srand( (unsigned) time(&t) );       // initialize the random number generator
+        int randomNumber =  rand() % 21;    // Get random number from 0-20
         
-        printf("The name of the game is: Guess the number!\n\n You  have 5 attempts to guess the correct whole number between 0-20\n\n What is your first attempt?\n");
-        printf("Remaining guesses: %d\n", remainingGuesses);
+        printf("\nGUESS THE CORRECT NUMBER!\n\n The game is simple, guess the correct whole between 0-20\n\n Anything other than numbers will be ignored! \n\n What is the first guess?\n\n");
+        printf("Remaining guesses: %d\n", guessCount);
  
 
-                 //While loop to check user nuber of guesses //
-                while(guessCount > 0) {
+        //** While loop to check user nuber of guesses **//
+        
+        while(guessCount > 0) {
                             
-                                    //***  Conditional to verify an integer is being entered   ****//
-                                     
-                                     
-                                     
-                                     if (scanf("%f", &tempNum) == 1) {      // checks whether scanf successfully reads an integer
-                                                      if((int)tempNum == tempNum) {
-                                                          userNumber = (int)tempNum;
-                                                          
-                                                          
-                //                                        printf("Invalid input. Please enter a valid number.\n");
-                //                                        while (getchar() != '\n'); // clear the input buffer
-                //                                        continue;
-                //                            
+                        //***  Conditional to verify an integer is being entered   ***//
                                     
+                        if (scanf("%f", &tempNum) == 1) {                       //Checks whether scanf successfully reads an integer
                                             
-                                            // Conditional to check if the number entered is between 0 and 20;
-                        
-                                            if (userNumber>= 0 && userNumber <=20) {
-                                                        --guessCount;
-                                                        --remainingGuesses; // Subtract one guess per loop
-                                                        if(guessCount == 0 && userNumber!=randomNumber ) {  // needs the && operator to prevent game over even if correct
-                                                                    printf("Game over, You Suck!");
-                                                                    break;
-                                                                
-                                                            }
-                                
-                                                        if (userNumber == randomNumber) {
+                                            if( (int)tempNum == tempNum ) {     //Checks if the input is a whole number
                                                 
-                                                                    printf("Correct,  You Win! user number: %d \n My number: %d", userNumber, randomNumber);
-                                                                    break;
-                                                        } else {
+                                                    userNumber = (int)tempNum;  //If true, the value is stored in userNumber
+                                                          
+                                                          
+                                                    //*** Conditional to check if the number entered is between 0 and 20;   ***//
+                                
+                                                    if (userNumber>= 0 && userNumber <=20) {
+                                                        
+                                                                --guessCount;   // decrements 1 per execution if value is 0-20
+                                                        
+                                                                if (guessCount == 0 && userNumber!=randomNumber ) {  // needs the && operator to prevent user from loosing even if correct
+
+                                                                            printf("Game over, You Suck!");
+                                                                            break;
+                                                                
+                                                                }
+                                
+                                                                //*** Conditional to end game upon correct guess ***//
+                                                                
+                                                                if (userNumber == randomNumber) {
+                                                
+                                                                            printf("\nYou Win! \n\n My number is: %d\n", randomNumber);
+                                                                            break;
+                                                                } else {
                  
-                                                                        
-                                                                    printf("Attempt remaining %d\n", remainingGuesses);
+                                                                            printf("Attempts remaining %d\n\n", guessCount);
                                                                      
-                                                                            if (userNumber> randomNumber) {
+                                                                            if (userNumber > randomNumber) {
                                                                                     
-                                                                                    printf("The number you guessed is too high\n");
-                                                                                    
-                                                                                    printf("My number: %d\n", randomNumber);
-                                                                                    printf("Your Number: %d\n", userNumber);
+                                                                                    printf("My number is lower!\n");
+                                                                                    while (getchar() != '\n') {} // Clear input buffer
                                             
                                                                             }  else {
                                                                             
-                                                                                    printf("The number you guessed is too low\n");
-                                                                                    printf("My number: %d\n", randomNumber);
-                                                                                    printf("Your number: %d \n", userNumber);
-
+                                                                                    printf("My number is higher!\n");
+                                                                                    while (getchar() != '\n') {} // Clear input buffer
+                    
                                                                             }
                                                         
-                                                } 
+                                                                } 
                                                 
                                     
+                                                    } else {
+                                
+                                                            printf("Error: Enter a number between 0-20!");
+                                
+                                                    }
+        
                                             } else {
-                                
-                                                        printf("Error! the number entered is not an acceptable number between 0-20!");
-                                
-                                                    }        
-                                        } else {
-                                                    printf("Error: Decimal number entered. Please enter a whole number.\n");
+                                                
+                                                    printf("Error: Decimal number entered. Enter a whole number.\n");
                                                     while (getchar() != '\n') {} // Clear input buffer
                                             
                                             }
                                     
-                            } else {
+                        } else {
+                            
                                     printf("Error: Invalid input. Please enter a valid whole number.\n");
                                     while (getchar() != '\n') {} // Clear input buffer
-                                    }
+                                    
+                        }
                     
-                    
-                }
-
+        }
 
         return 0;
     
